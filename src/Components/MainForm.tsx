e450672e6f987cbd09e818e33f7bc34ccd0ca118
iamtheef <eftihis.riise@gmail.com>
2020-11-13 21:50:45 +0200

    some hooks

--- src/Components/MainForm.tsx
+++ src/Components/MainForm.tsx
@@ -2,6 +2,13 @@ import React, { FC, useState } from "react";
 import { InputGroup, FormControl, Container, Row, Form } from "react-bootstrap";
 
 export const MainForm: FC = () => {
+  const [file, setFile] = useState(null);
+  const [filename, setFilename] = useState("or upload an image");
+
+  const handleChange = (e: any) => {
+    setFile(e.target.files[0]);
+    setFilename(e.target.files[0].name);
+  };
   return (
     <Container>
       <Row className="main-form">
@@ -12,7 +19,12 @@ export const MainForm: FC = () => {
             aria-describedby="basic-addon2"
           />
 
-          <Form.File label="or upload a file" lang="en" custom />
+          <Form.File
+            label={filename}
+            lang="en"
+            custom
+            onChange={handleChange}
+          />
         </InputGroup>
       </Row>
     </Container>
